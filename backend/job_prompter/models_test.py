cf875e85408f04b4d36d83ee3f3ef83137d5267a
tobi adewunmi <tobyadey@gmail.com>
2023-06-21 15:09:11 +0100

    fixed make install issues

---
+++ backend/job_prompter/models_test.py
@@ -0,0 +1,10 @@
+# import pytest
+# from flask import json
+
+# from job_prompter import config
+# from job_prompter.extensions import db as _db
+# from job_prompter.factory import create_app
+
+
+def test():
+    assert True
