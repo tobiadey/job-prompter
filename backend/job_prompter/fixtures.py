066ddd7e57cf10fd9355ad3b8c4f23bf36122a05
tobi adewunmi <tobyadey@gmail.com>
2023-06-21 17:53:39 +0100

    adding constant EXAMPLE_USER

--- backend/job_prompter/fixtures.py
+++ backend/job_prompter/fixtures.py
@@ -1,10 +1,17 @@
 from job_prompter.extensions import db
 from job_prompter.models import User
 
+EXAMPLE_USER = {
+    "first_name": "jeff",
+    "last_name": "king",
+    "email": "jking@gmail.com",
+    "password": "hash254",
+}
+
 
 def create_user(**kwargs):
     defaults = dict(
-        first_name="Jeff", last_name="King", email="jking@gmail.com", password="hash254"
+        first_name="jeff", last_name="king", email="jking@gmail.com", password="hash254"
     )
 
     user = User(**{**defaults, **kwargs})
